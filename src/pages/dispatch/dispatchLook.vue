4e4f4063db636767043966bd8c4f47b36d2d4758
Gsn11 <1058664895@qq.com>
2023-03-02 10:54:47 +0800

    bug

--- src/pages/dispatch/dispatchLook.vue
+++ src/pages/dispatch/dispatchLook.vue
@@ -11,16 +11,16 @@
       <ListRow isBorder title="分组名称" :content="selectDeviceData.groupName" />
       <ListRow isBorder title="运用类型" :content="selectDeviceData.typeName" />
       <ListRow isBorder title="购买日期" :content="selectDeviceData.purchaseDate" />
-      <ListRow isBorder title="保修截至日期" :content="selectDeviceData.warrantyDate" />
       <ListRow isBorder title="安装日期" :content="selectDeviceData.deviceDate" />
+      <ListRow isBorder title="设备状态" :content="setStateType(selectDeviceData.deviceStatus)" />
+      <ListRow isBorder title="保修截至日期" :content="selectDeviceData.warrantyDate" />
       <ListRow isBorder title="上次保养时间" :content="selectDeviceData.maintainLastData" />
       <ListRow isBorder title="下次保养时间" :content="selectDeviceData.maintainNextDate" />
-      <ListRow isBorder title="设备状态" :content="setStateType(selectDeviceData.deviceStatus)" />
-      <ListRow isBorder title="制造商" :content="selectDeviceData.manufacturer" />
-      <ListRow isBorder title="保修期" :content="selectDeviceData.warranty + selectDeviceData.warrantyUnit" />
       <ListRow isBorder title="状态持续时间" :content="selectDeviceData.runTimer" />
       <ListRow isBorder title="保养周期(天)" :content="selectDeviceData.maintainDays" />
       <ListRow isBorder title="使用年限(月)" :content="selectDeviceData.useYear" />
+      <ListRow isBorder title="制造商" :content="selectDeviceData.manufacturer" />
+      <ListRow isBorder title="保修期" :content="selectDeviceData.warranty + selectDeviceData.warrantyUnit" />
       <ViewImage :data="returnImgs(selectDeviceData.installImg)" />
       <uni-list>
         <uni-list-item title="历史记录" showArrow clickable @click="onClickToDevHistory()"></uni-list-item>
